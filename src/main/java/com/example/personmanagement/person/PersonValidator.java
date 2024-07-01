b526efd6a7db3eb06bd20e2dc91a1350d7572342
bm09 <borys.mankowski@gmail.com>
2024-07-01 18:54:40 +0200

    poprawka_10

--- src/main/java/com/example/personmanagement/person/PersonValidator.java
+++ src/main/java/com/example/personmanagement/person/PersonValidator.java
@@ -5,9 +5,6 @@ import com.example.personmanagement.person.model.Person;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 @Component
 @RequiredArgsConstructor
 public class PersonValidator {
@@ -17,14 +14,5 @@ public class PersonValidator {
         if (personRepository.existsByPesel(person.getPesel())) {
             throw new DuplicateResourceException("Person with this PESEL already exists!");
         }
-    }
-
-    public void validatePersonsForBatchSave(List<Person> persons) {
-        persons.forEach(person -> {
-            Stream<Person> personsWithSamePesel = persons.stream().filter(p -> person.getPesel().equals(p.getPesel()));
-            if (personsWithSamePesel.count() > 1) {
-                throw new DuplicateResourceException("Person with this PESEL already exists!");
-            }
-        });
     }
 }
