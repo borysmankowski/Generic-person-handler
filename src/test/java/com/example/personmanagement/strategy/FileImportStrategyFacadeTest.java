8a35148609e33f8d02dec4bb209092d1ea179410
bm09 <borys.mankowski@gmail.com>
2024-07-19 14:24:22 +0200

    poprawka-12

---
+++ src/test/java/com/example/personmanagement/strategy/FileImportStrategyFacadeTest.java
@@ -0,0 +1,57 @@
+package com.example.personmanagement.strategy;
+
+import com.example.personmanagement.exception.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileImportStrategyFacadeTest {
+    @Mock
+    private PersonFileImportStrategy mockStrategy;
+
+    private FileImportStrategyFacade strategyFacade;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        Map<String, PersonFileImportStrategy> strategyMap = new HashMap<>();
+        strategyMap.put("EMPLOYEE", mockStrategy);
+        strategyMap.put("PENSIONER", mockStrategy);
+        strategyMap.put("STUDENT", mockStrategy);
+
+        strategyFacade = new FileImportStrategyFacade(strategyMap);
+    }
+
+    @Test
+    public void testGetStrategy_ValidKeyEMPLOYEE() {
+        String key = "EMPLOYEE";
+        PersonFileImportStrategy strategy = strategyFacade.getStrategy(key);
+        assertEquals(mockStrategy, strategy);
+    }
+
+    @Test
+    public void testGetStrategy_ValidKeySTUDENT() {
+        String key = "STUDENT";
+        PersonFileImportStrategy strategy = strategyFacade.getStrategy(key);
+        assertEquals(mockStrategy, strategy);
+    }
+
+    @Test
+    public void testGetStrategy_ValidKeyPENSIONER() {
+        String key = "PENSIONER";
+        PersonFileImportStrategy strategy = strategyFacade.getStrategy(key);
+        assertEquals(mockStrategy, strategy);
+    }
+
+    @Test
+    public void testGetStrategy_InvalidKey() {
+        String key = "invalid";
+        assertThrows(ResourceNotFoundException.class, () -> strategyFacade.getStrategy(key));
+    }
+}
---
+++ src/test/java/com/example/personmanagement/strategy/csv/StudentFileImportStrategyTest.java
@@ -0,0 +1,67 @@
+package com.example.personmanagement.strategy.csv;
+
+import com.example.personmanagement.exception.InvalidStrategyTypeException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class StudentFileImportStrategyTest {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private StudentFileImportStrategy studentFileImportStrategy;
+
+    private List<String[]> validData;
+    private List<String[]> invalidData;
+
+    @BeforeEach
+    void setUp() {
+        validData = List.of(
+                new String[]{"STUDENT", "John", "Doe", "1234567890", "180.5", "75.0", "john.doe@example.com", "University of Example", "2", "Computer Science", "500.0"},
+                new String[]{"STUDENT", "Jane", "Doe", "0987654321", "165.2", "60.0", "jane.doe@example.com", "Example University", "1", "Mathematics", "600.0"}
+        );
+
+        invalidData = List.of(
+                new String[]{"INVALID", "John", "Doe", "1234567890", "180.5", "75.0", "john.doe@example.com", "University of Example", "2", "Computer Science", "500.0"},
+                new String[]{"INVALID", "Jane", "Doe", "0987654321", "165.2", "60.0", "jane.doe@example.com", "Example University", "1", "Mathematics", "600.0"}
+        );
+    }
+
+    @Test
+    public void testBulkInsert_withValidData() {
+        studentFileImportStrategy.bulkInsert(validData, jdbcTemplate);
+
+        verify(jdbcTemplate).batchUpdate(anyString(), anyList());
+    }
+
+    @Test
+    public void testBulkInsert_withInvalidData_shouldThrowException() {
+        assertThrows(InvalidStrategyTypeException.class, () -> {
+            studentFileImportStrategy.bulkInsert(invalidData, jdbcTemplate);
+        });
+    }
+
+    @Test
+    public void testBulkInsert_whenJdbcTemplateThrowsException() {
+        doThrow(RuntimeException.class).when(jdbcTemplate).batchUpdate(anyString(), anyList());
+
+        assertThrows(RuntimeException.class, () -> {
+            studentFileImportStrategy.bulkInsert(validData, jdbcTemplate);
+        });
+    }
+}
